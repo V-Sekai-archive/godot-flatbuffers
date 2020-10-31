c06340bbcc045430165ab948c26a8087ac47a775
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-30 17:55:43 -0700

    Align names,

--- resource_importer_flexbuffer.h
+++ resource_importer_flexbuffer.h
@@ -215,10 +215,10 @@ protected:
 
 		ClassDB::bind_method(D_METHOD("set_data", "data"), &FlexbuffersData::set_data);
 		ClassDB::bind_method(D_METHOD("get_data"), &FlexbuffersData::get_data);
-		ClassDB::bind_method(D_METHOD("set_flexbuffer", "flexbuffers"), &FlexbuffersData::set_flexbuffers);
-		ClassDB::bind_method(D_METHOD("get_flexbuffer"), &FlexbuffersData::get_flexbuffers);
+		ClassDB::bind_method(D_METHOD("set_flexbuffers", "flexbuffers"), &FlexbuffersData::set_flexbuffers);
+		ClassDB::bind_method(D_METHOD("get_flexbuffers"), &FlexbuffersData::get_flexbuffers);
 
-		ADD_PROPERTY(PropertyInfo(Variant::NIL, "data", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_NIL_IS_VARIANT), "set_data", "get_data");
+		ADD_PROPERTY(PropertyInfo(Variant::NIL, "data", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_NIL_IS_VARIANT), "set_data", "get_data");
 	}
 
 public:
